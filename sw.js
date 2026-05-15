c70f3b1e8096ae437b53a1edd8c36c5e46761128
Tido Willems <196117489+TidoWillems@users.noreply.github.com>
2026-05-16 00:23:08 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'droidex-v1'
+const CACHE = 'droidex-v2'
 
 self.addEventListener('install', (event) => {
   self.skipWaiting()
@@ -14,7 +14,15 @@ self.addEventListener('install', (event) => {
 })
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim())
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  )
 })
 
 self.addEventListener('fetch', (event) => {
