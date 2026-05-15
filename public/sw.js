7f633c3870bf2a7444661dd08edd256da678b1ef
Tido Willems <196117489+TidoWillems@users.noreply.github.com>
2026-05-15 22:04:29 +0200

    Enable offline cache for PWA install

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,26 @@
-self.addEventListener('install', () => {
+const CACHE = 'droidex-v1'
+
+self.addEventListener('install', (event) => {
   self.skipWaiting()
+
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([
+        '/droidex/',
+        '/droidex/manifest.webmanifest',
+      ])
+    })
+  )
 })
 
-self.addEventListener('activate', () => {
-  console.log('SW active')
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request)
+    })
+  )
 })
