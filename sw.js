51cd138e59a56111921be6b841084fc4edac60a6
Tido Willems <196117489+TidoWillems@users.noreply.github.com>
2026-05-19 20:18:01 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,53 +1,39 @@
 const CACHE = 'droidex-v5';
 
 self.addEventListener('install', (event) => {
-
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE)
+    caches
+      .open(CACHE)
       .then((cache) =>
         cache.addAll([
           '/droidex/',
           '/droidex/manifest.webmanifest',
           '/droidex/icon-192.png',
-          '/droidex/icon-512.png'
+          '/droidex/icon-512.png',
         ])
       )
   );
 });
 
 self.addEventListener('activate', (event) => {
-
   event.waitUntil(
-
-    caches.keys()
-      .then(keys =>
-
+    caches
+      .keys()
+      .then((keys) =>
         Promise.all(
-          keys
-            .filter(key => key !== CACHE)
-            .map(key => caches.delete(key))
+          keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))
         )
-
       )
       .then(() => self.clients.claim())
-
   );
-
 });
 
 self.addEventListener('fetch', (event) => {
-
   event.respondWith(
-
-    caches.match(event.request)
-      .then((cached) => {
-
-        return cached || fetch(event.request);
-
-      })
-
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
   );
-
 });
